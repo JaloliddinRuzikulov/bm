6d7a2221d826ad85b8ade9514151c9b21ab13191
jaloliddinruzikulov <jaloliddinruzikulov@gmail.com>
2023-02-18 00:34:45 +0500

    Add url

--- waltalkio/urls.py
+++ waltalkio/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
 urlpatterns = [
-
+    path('depot/', ListDepot.as_view(), name='list_depot'),
+    path('depot/add/', AddDepot.as_view(), name='add_depot'),
+    path('depot/out/', OutDepot.as_view(), name='out_depot'),
+    path('depot/out/<int:pk>/print/', OutDepotPrint.as_view(), name='out_depot_print'),
+    path('depot/out/<int:pk>/close/', OutDepotClose.as_view(), name='out_depot_close'),
 ]
