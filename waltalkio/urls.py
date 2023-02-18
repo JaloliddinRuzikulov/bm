ecc46b58984a2731056db000e8b621f45b52d2d1
jaloliddinruzikulov <jaloliddinruzikulov@gmail.com>
2023-02-18 20:14:40 +0500

    bug fixes

--- waltalkio/urls.py
+++ waltalkio/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
+from .views import AddView
 urlpatterns = [
     # path('/', ListDepot.as_view(), name='list_depot'),
     # path('/<int:id>', ListDepot.as_view(), name='list_depot'),
-    # path('/add', AddDepot.as_view(), name='add_depot'),
+    path('add/', AddView.as_view(), name='add_walktalkio'),
     # path('/out/', OutDepot.as_view(), name='out_depot'),
 ]
--- waltalkio/views.py
+++ waltalkio/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from warehouse.models import Catalog, ModelProduct, Depot
+import datetime
+
 
 # Create your views here.
+class AddView(LoginRequiredMixin, TemplateView):
+    template_name = 'depot.html'
+
+    def post(self, request):
+        data = request.POST
+        catalog, created_catalog = Catalog.objects.get_or_create(name=data['katalog'])
+        model, created_model = ModelProduct.objects.get_or_create(catalog=catalog, name=data['model'])
+        for i in range(1, int(data['counts']) + 1):
+            Depot.objects.get_or_create(model=model, qr_code=data["field" + str(i)], came_date=datetime.date.today())
+        return redirect('add_depot')
