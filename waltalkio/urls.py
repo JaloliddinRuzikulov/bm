1d2dbb951d3f949a397e303636a99290e72f33ef
jaloliddinruzikulov <jaloliddinruzikulov@gmail.com>
2023-02-18 19:59:45 +0500

    bug fixes

--- waltalkio/urls.py
+++ waltalkio/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
 urlpatterns = [
-    # path('depot/', ListDepot.as_view(), name='list_depot'),
-    # path('depot/add/', AddDepot.as_view(), name='add_depot'),
-    # path('depot/out/', OutDepot.as_view(), name='out_depot'),
-    # path('depot/out/<int:pk>/print/', OutDepotPrint.as_view(), name='out_depot_print'),
-    # path('depot/out/<int:pk>/close/', OutDepotClose.as_view(), name='out_depot_close'),
+    # path('/', ListDepot.as_view(), name='list_depot'),
+    # path('/<int:id>', ListDepot.as_view(), name='list_depot'),
+    # path('/add', AddDepot.as_view(), name='add_depot'),
+    # path('/out/', OutDepot.as_view(), name='out_depot'),
 ]
