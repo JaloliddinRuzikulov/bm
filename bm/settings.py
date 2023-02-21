4a83db762809ff342124d99f4226f8fdf4419181
jaloliddinruzikulov <jaloliddinruzikulov@gmail.com>
2023-02-21 16:37:53 +0500

    change database postgresql

--- bm/settings.py
+++ bm/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bm',
+        'USER': 'bmuser',
+        'PASSWORD': 'qwerty123',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
