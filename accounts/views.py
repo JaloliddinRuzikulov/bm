20f735c006c3b111aecc545f5a0fac79cd7e8cbf
Jaloliddin1Ruzikulov <jaloliddinruzikulov@gmail.com>
2023-04-05 17:39:28 +0500

    fixes aside template

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-
 # Create your views here.
+
 
 class Profile(LoginRequiredMixin, TemplateView):
     template_name = 'registration/profile.html'
